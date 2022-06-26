dfae9a2d89b15ce3cd2d4a3acb033514a5beec6d
ritik94155 <ritik94155@gmail.com>
2022-06-27 00:58:05 +0530

    initial-commit

---
+++ src/data.js
@@ -0,0 +1,45 @@
+const list = [
+  {
+    id: 1,
+    title: "Nord Stage 3 88 Keys",
+    author: "by Wonder House Books | 25 April 2018",
+    price: "$5,738.00",
+    img: "https://i.pinimg.com/564x/11/1a/48/111a486b7e7c520b0fd808cb32cbb3ba.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "Line 6 helix rauk Guitar Processor - 1x opened box",
+    author: "by Maple Press  | 1 September 2020",
+    price: "$1,831.57",
+    img: "https://ddbq1elkirlzo.cloudfront.net/m/6274/x-large/6RJuBrjaedRuC4CoUyItA1ATkr7uI8KalVMMwO7A.jpg",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Nord Lead A1",
+    author: "by Om Books Editorial Team  | 25 November 2018",
+    price: "$1700",
+    img: "https://www.nordkeyboards.com/sites/default/files/files/products/nord-lead-a1/images/Nord-Lead-A1-angled-white_xl.jpg",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "Moog Subsequent 37",
+    author: "by Wonder House Books | 25 April 2018",
+    price: "$1700",
+    img: "https://www.sudeepaudio.com/saProImages/300x300/moogmusic/subseq-2.jpg",
+    amount: 1,
+  },
+  {
+    id: 5,
+    title: "Moog Minitaur 2010s Black",
+    author: "by Wonder House Books  | 1 January 2018",
+    price: "$682.80",
+    img: "http://www.audiocentralmagazine.com/wp-content/uploads/2012/04/Minitaur-opening-300x215.png",
+    amount: 1,
+  },
+
+];
+
+export default list;
